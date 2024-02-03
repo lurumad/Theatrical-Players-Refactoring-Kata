a89d2ed1e408192c73c2360d6a34dddb6f7d7a73
lurumad <luisruizpavon@gmail.com>
2024-02-03 08:42:45 +0100

    refactor: move methods to performance improving cohesion

--- kotlin/src/main/kotlin/Performance.kt
+++ kotlin/src/main/kotlin/Performance.kt
@@ -1,1 +1,62 @@
-data class Performance(val playID: String, val audience: Int)
+import kotlin.math.floor
+import kotlin.math.max
+
+data class Performance(val playID: String, val audience: Int) {
+
+    fun credits(play: Play): Credits {
+        var performanceCredits = Credits(max(audience - 30, 0))
+        performanceCredits = performanceCredits.add(creditsByGenre(play))
+        return performanceCredits
+    }
+
+    fun amount(play: Play): Amount {
+        if (play.type == "tragedy") {
+            return amountByTragedy()
+        }
+
+        if (play.type == "comedy") {
+            return amountByComedy()
+        }
+
+        throw Error("unknown type: {play.type}")
+    }
+
+    private fun amountByComedy(): Amount {
+        var performanceAmount = Amount(30000)
+        performanceAmount = performanceAmount.add(comedyExtraAmountByAudience())
+        return performanceAmount.add(comedyExtraAmountByGenre())
+    }
+
+    private fun amountByTragedy(): Amount {
+        var performanceAmount = Amount(40000)
+        performanceAmount = performanceAmount.add(extraAmountByAudience())
+        return performanceAmount.add(tragedyExtraAmountByGenre())
+    }
+
+    private fun extraAmountByAudience(): Amount {
+        if (audience > 30) {
+            return Amount(1000 * (audience - 30))
+        }
+        return Amount(0)
+    }
+
+    private fun comedyExtraAmountByAudience(): Amount {
+        if (audience > 20) {
+            return Amount(10000 + 500 * (audience - 20))
+        }
+        return Amount(0)
+    }
+
+    private fun comedyExtraAmountByGenre() = Amount(300 * audience)
+
+    private fun tragedyExtraAmountByGenre(): Amount {
+        return Amount(0)
+    }
+
+    private fun creditsByGenre(play: Play): Credits {
+        if ("comedy" == play.type) {
+            return Credits(floor((audience / 5).toDouble()).toInt())
+        }
+        return Credits(0)
+    }
+}
--- kotlin/src/main/kotlin/StatementPrinter.kt
+++ kotlin/src/main/kotlin/StatementPrinter.kt
@@ -1,7 +1,5 @@
 import java.text.NumberFormat
 import java.util.Locale
-import kotlin.math.floor
-import kotlin.math.max
 
 class StatementPrinter {
 
@@ -14,73 +12,27 @@ class StatementPrinter {
 
         invoice.performances.forEach { performance ->
             val play = catalog.playBy(performance.playID)
-            val performanceAmount: Amount = performanceAmount(performance, play)
-            val performanceCredits = performanceCredits(performance, play)
 
-            invoiceCredits = invoiceCredits.add(performanceCredits)
-            invoiceAmount = invoiceAmount.add(performanceAmount)
+            invoiceCredits = invoiceCredits.add(performance.credits(play))
+            invoiceAmount = invoiceAmount.add(performance.amount(play))
 
-            result += "  ${play.name}: ${format((performanceAmount.usd()).toLong())} (${performance.audience} seats)\n"
+            result += "  ${play.name}: ${format((performance.amount(play).usd()).toLong())} (${performance.audience} seats)\n"
         }
         result += "Amount owed is ${format((invoiceAmount.usd()).toLong())}\n"
         result += "You earned $invoiceCredits credits\n"
         return result
     }
 
-    private fun performanceCredits(performance: Performance, play: Play): Credits {
-        var performanceCredits = Credits(max(performance.audience - 30, 0))
-        performanceCredits = performanceCredits.add(performanceCreditsByGenre(performance, play))
-        return performanceCredits
-    }
 
-    private fun performanceCreditsByGenre(performance: Performance, play: Play): Credits {
-        if ("comedy" == play.type) {
-            return Credits(floor((performance.audience / 5).toDouble()).toInt())
-        }
-        return Credits(0)
-    }
 
-    private fun performanceAmount(perf: Performance, play: Play): Amount {
-        if (play.type == "tragedy") {
-            return performanceAmountByTragedy(perf)
-        }
 
-        if (play.type == "comedy") {
-            return performanceAmountByComedy(perf)
-        }
 
-        throw Error("unknown type: {play.type}")
-    }
 
-    private fun performanceAmountByComedy(perf: Performance): Amount {
-        var performanceAmount = Amount(30000)
-        performanceAmount = performanceAmount.add(comedyExtraAmountByAudience(perf))
-        return performanceAmount.add(comedyExtraAmountByGenre(perf))
-    }
 
-    private fun performanceAmountByTragedy(perf: Performance): Amount {
-        var performanceAmount = Amount(40000)
-        performanceAmount = performanceAmount.add(tragedyExtraAmountByAudience(perf))
-        return performanceAmount.add(tragedyExtraAmountByGenre(perf))
-    }
 
-    private fun tragedyExtraAmountByGenre(perf: Performance): Amount {
-        return Amount(0)
-    }
 
-    private fun comedyExtraAmountByGenre(perf: Performance) = Amount(300 * perf.audience)
 
-    private fun comedyExtraAmountByAudience(perf: Performance): Amount {
-        if (perf.audience > 20) {
-            return Amount(10000 + 500 * (perf.audience - 20))
-        }
-        return Amount(0)
-    }
 
-    private fun tragedyExtraAmountByAudience(perf: Performance): Amount {
-        if (perf.audience > 30) {
-            return Amount(1000 * (perf.audience - 30))
-        }
-        return Amount(0)
-    }
+
+
 }
